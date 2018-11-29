43c58a3dd13296aaa391afb604dbd2c6183e26dd
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-29 15:05:07 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+ 
+   
+  def keys_of(*arguments)
     # code goes here
+    hash_keys = self.keys
+    binding.pry 
+    newA = []
+    arguments
+    hash_keys.each do |x|
+      arguments.each do |y|
+        x == y ? newA.push(x) : nil  
+      end
+    end
+    newA
   end
+   
 end
